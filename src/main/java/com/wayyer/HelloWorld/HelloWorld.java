7ad0f8a7da3c2fc210b463ab12a2003db321c0d6
wayyer <wcao12@its.jnj.com>
2019-04-10 14:34:16 +0800

    init the project 'Hello World'

---
+++ src/main/java/com/wayyer/HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+package main.java.com.wayyer.HelloWorld;
+
+/**
+ * @Author: wayyer
+ * @Description: hello world
+ * @Program: HelloWorld
+ * @Date: 2018.12.10
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+
+}
